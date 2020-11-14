5d44d52c55d3d9da354616b87b89ea1cab36b9fe
alestor123 <alestoraldous@gmail.com>
2020-11-14 15:57:52 +0530

    added main route

--- App.js
+++ App.js
@@ -4,5 +4,10 @@ var express = require('express'),
 app = express(),
 port = process.env.PORT || process.argv[2] || 3000;
 app.use(express.json());
-
+app.get('/', (req, res) => {
+    res.json({'info':'This is the octodays'})
+})
+app.get('/octodex', (req, res) => {
+    res.json({'info':'This is the octodays'})   
+})
 app.listen(port, () => console.log(`server running at ${port}`))
