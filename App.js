74f574f416689d17ff797d3ed6e5585477fa2ea2
alestor123 <alestoraldous@gmail.com>
2020-11-14 15:54:48 +0530

    added port listner

--- App.js
+++ App.js
@@ -3,3 +3,4 @@ require('dotenv').config()
 var express = require('express'),
 app = express(),
 port = process.env.PORT || process.argv[2] || 3000;
+app.listen(port, () => console.log(`server running at ${port}`))
