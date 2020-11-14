6703530c04b18da981a60876721fab86c705d38f
alestor123 <alestoraldous@gmail.com>
2020-11-14 17:40:12 +0530

    added cli

--- App.js
+++ App.js
@@ -9,11 +9,18 @@ argv = process.argv[2],
 port = process.env.PORT || argv || 3000;
 app.use(express.json());
 app.use(favicon(path.join(__dirname, 'assets/icon/icon.png')))
-if(argv== '-v' || '--version'){
+if(argv== '-v' ||argv == '--version'){
     console.log( `${pck.version}`)
   process.exit(1);
-  }
-
+}
+else if (argv =='-h'|| argv == '--help') { // checking undifined args
+    console.log(`
+    Usage: octodays <Port> 
+`);
+}
+else{
+    app.listen(port, () => console.log(`server running at ${port}`))
+}
 app.get('/', (req, res) => {
     res.json({'info':'This is the octodays'})
 })
@@ -21,4 +28,3 @@ app.get('/octodex', (req, res) => {
     var day = new Date().getDay()
     res.contentType('image/png').header('Cache-Control', 'no-cache,max-age=600').sendFile(path.resolve(path.resolve(__dirname,`assets/octets/${day}.png`)));
 })
-app.listen(port, () => console.log(`server running at ${port}`))
