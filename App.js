a538f7de941ecf601c4de07b239a74548deb2def
alestor123 <alestoraldous@gmail.com>
2020-11-14 16:31:20 +0530

    added day

--- App.js
+++ App.js
@@ -2,12 +2,17 @@
 require('dotenv').config()
 var express = require('express'),
 app = express(),
+path = require('path')
+favicon = require('serve-favicon'),
 port = process.env.PORT || process.argv[2] || 3000;
 app.use(express.json());
+app.use(favicon(path.join(__dirname, 'assets/icon/icon.png')))
 app.get('/', (req, res) => {
     res.json({'info':'This is the octodays'})
 })
 app.get('/octodex', (req, res) => {
-    res.json({'info':'This is the octodays'})   
+    var day = new Date().getDay()
+    res.contentType('image/png').header('Cache-Control', 'no-cache,max-age=600').sendFile(path.resolve(path.resolve(__dirname,`assets/octets/${day}.png`)));
+
 })
 app.listen(port, () => console.log(`server running at ${port}`))
