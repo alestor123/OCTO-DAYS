1b197609ccd97418dc6b54d19a8531cc0754bafc
alestor123 <alestoraldous@gmail.com>
2020-11-14 15:55:19 +0530

    json middleware

--- App.js
+++ App.js
@@ -3,4 +3,6 @@ require('dotenv').config()
 var express = require('express'),
 app = express(),
 port = process.env.PORT || process.argv[2] || 3000;
+app.use(express.json());
+
 app.listen(port, () => console.log(`server running at ${port}`))
